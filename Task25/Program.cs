5495276facd39f8df638bad7c5eafa375f9e1574
Kseniya <ksavierra@rambler.ru>
2022-07-16 19:06:01 +0500

    Изменеия в задаче 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,31 +2,16 @@
 static void Main(){
 
 
-//   if (number1 <= 0 || number2 <= 0)
-//      Console.WriteLine("Возможен ввод только натуральных чисел");
-//else
-//{
-//   Console.WriteLine(Math.Pow(number1, number2));
-//}
-   
     Console.WriteLine("Введите число (A): ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число (B): ");
 int number2 = Convert.ToInt32(Console.ReadLine());
   
-//   if (number1 <= 0 || number2 <= 0)
-//      Console.WriteLine("Возможен ввод только натуральных чисел");
-//else
-//{
-//   Console.WriteLine(Math.Pow(number1, number2));
-//}
-
 switch (number1 <= 0 || number2 <= 0)
 {
   case true:
     Console.WriteLine("Возможен ввод только натуральных чисел");
-    // Запуск метода Main, если условие некорректно
-    Main();
+        Main();
     break;
   case false:
     Console.WriteLine(Math.Pow(number1, number2));
