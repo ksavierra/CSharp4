2e40c121610c931276a79095eb202ba0d450cd27
Kseniya <ksavierra@rambler.ru>
2022-07-16 21:55:54 +0500

    Решение задачи 27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+        
+            int sum = 0;
+            while(number!=0)
+            {
+                sum = sum + number % 10;
+                number = number / 10;
+            }
+            
+            Console.WriteLine("Сумма чисел равна: {0}",sum);
+            Console.ReadKey();
