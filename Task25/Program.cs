4d9347dc2aff8897a8351c111b1fbc3d5af99c99
Kseniya <ksavierra@rambler.ru>
2022-07-16 18:43:43 +0500

    Решение задачи 25

---
+++ Task25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+static void Main(){
+
+
+//   if (number1 <= 0 || number2 <= 0)
+//      Console.WriteLine("Возможен ввод только натуральных чисел");
+//else
+//{
+//   Console.WriteLine(Math.Pow(number1, number2));
+//}
+   
+    Console.WriteLine("Введите число (A): ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число (B): ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+  
+//   if (number1 <= 0 || number2 <= 0)
+//      Console.WriteLine("Возможен ввод только натуральных чисел");
+//else
+//{
+//   Console.WriteLine(Math.Pow(number1, number2));
+//}
+
+switch (number1 <= 0 || number2 <= 0)
+{
+  case true:
+    Console.WriteLine("Возможен ввод только натуральных чисел");
+    // Запуск метода Main, если условие некорректно
+    Main();
+    break;
+  case false:
+    Console.WriteLine(Math.Pow(number1, number2));
+    break;
+}   
+}
+Main();
